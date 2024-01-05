9fa194daf7151bb2503333926381b156c3aaf3c0
Sakib_Dalal <sakibdalal73@gmail.com>
2024-01-05 23:00:57 +0530

    Stage 2 - add input text input, shape, (x/y) position

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import turtle
 my_turtle = turtle.Turtle()
 my_screen = turtle.Screen()
 
-my_screen.setup(500, 400)
+my_screen.setup(width=500, height=400)
 # width = 500, height = 400
+
+user_bet = my_screen.textinput(title="Make a bet on turtle", prompt="Type turtle colour to make a bet: ")
+print(user_bet)
+
+my_turtle.shape("turtle")
+my_turtle.penup()
+my_turtle.goto(-230, 0)
 
 my_screen.exitonclick()
